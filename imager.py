3f8d2f191f050bba1b119605032dadfc6040545b
harisbasaric <basaricharis@gmail.com>
2020-04-21 20:49:24 +0200

    Adding first version of Imager

---
+++ imager.py
@@ -0,0 +1,90 @@
+# Written by Haris Basaric
+# Imager - Resize multiple images in one go
+# 13 April 2020
+
+# use PIL - Python Image Library, used for resizing the image and exporting the resized image
+from PIL import Image
+# Tkinter used for GUI
+import tkinter as tk
+import os
+import sys
+
+# importing specific functions for easier use
+# filedialog is prompt for opening and selecting folder
+# messagebox is for displaying info when process is finished
+from tkinter import filedialog
+from tkinter import messagebox
+
+# defining the main window object, setting the title, and windows size
+root = tk.Tk()
+root.title("Imager by Haris")
+root.geometry('400x100')
+
+# function that prints to console reduction percentage and selected folder
+# not used actually in the program currently, but it's useful for debugging
+def show_entry_fields():
+    print("Folder: %s\n Percentage: %s" % (folder_entry.get(), percentage_entry.get()))
+
+# function calld when "Load Folder" button is pressed
+# prompts file dialog where we select folder
+# inserts the path to selected folder into text box
+def load_folder():
+    folder_selected = filedialog.askdirectory()
+    folder_entry.insert(0, str(folder_selected))
+    print ("Folder selected: %s" % (folder_selected))
+
+# function that resizes every image in selected folder by reduction percentage
+def resize_image():
+    # gets the selected folder
+    # create a new one with same name + with suffix "_small"
+    # get the ratio from percentage
+    directory = folder_entry.get()
+    new_directory = directory + "_small"
+    os.mkdir(new_directory)
+    percent = int(percentage_entry.get())
+    percent /= 100
+    print ("Ratio: " + str(percent))
+
+    # for every image in selected folder
+    # open it
+    for file_name in os.listdir(directory):
+        print("Processing %s" % file_name)
+        image = Image.open(os.path.join(directory, file_name))
+
+        # get image dimensions
+        # and multiply the current dimensions with given ratio
+        # create new image object that will resize original image and set it to new dimensions
+        x,y = image.size
+        new_dimensions = (round(x*percent), round(y*percent))
+        output = image.resize(new_dimensions, Image.ANTIALIAS)
+
+        # make output file named "small_" + original file name and address it to new folder
+        # save the image to that location
+        output_file_name = os.path.join(new_directory, "small_" + file_name)
+        output.save(output_file_name, "JPEG", quality = 95)
+
+    # print info to console and show the message box if everything is ok
+    print("All done")
+    tk.messagebox.showinfo('Done', 'Operation successfully completed')
+
+# Below are GUI elements
+
+# Labels
+tk.Label(root, text="Picutres Folder ").grid(row=0)
+tk.Label(root, text="Resize percentage (%) ").grid(row=1)
+
+# Entries / text box input fields for folder name and percentage
+folder_entry = tk.Entry(root, width=40)
+percentage_entry = tk.Entry(root, width=10)
+
+# positioning entries
+folder_entry.grid(row=0, column=1)
+percentage_entry.grid(row=1, column=1)
+
+# two buttons for selection of folder and Resize
+# "Load Folder" triggers load_folder() fucntion
+# "Resize" triggers resize_image() function
+tk.Button(root, text='Load Folder', command=load_folder).grid(row=3, column=0, pady=10)
+tk.Button(root, text='Resize', command=resize_image).grid(row=3, column=1, pady=10)
+
+root.mainloop()
---
+++ imgres.py
@@ -0,0 +1,50 @@
+'''
+IMPORTANT NOTE:
+imgres is actually the subprogram of imager; Imager is the GUI version of imgres
+'''
+
+# Written by Haris Basaric
+# imgres - Resize multiple images in one go, command line interface
+# 26 November 2019
+
+# using PIL - Python Image Library
+from PIL import Image
+import os
+import sys
+
+#imgres my_pictures_folder
+
+# directory is the location where all photos are
+# program automatically creates new directory with additional "_small"
+# make system call to create new directory
+directory = sys.argv[1]
+new_directory = directory + "_small"
+os.mkdir(new_directory)
+
+# the process of getting the reduction percentage from user
+print ("\n IMAGE RESIZER BY HARIS BASARIC \n")
+percent = int(input("Please enter the % (1-99): "))
+percent /= 100
+print ("Ratio: " + str(percent))
+
+# for every file in the directory
+for file_name in os.listdir(directory):
+
+    # open the file as "image"
+    print("Processing %s" % file_name)
+    image = Image.open(os.path.join(directory, file_name))
+
+    # get the size of the image
+    # calculate the new dimensions based on current size and user-given percentage
+    # create "output" object that will be the resized original "image" object
+    x,y = image.size
+    new_dimensions = (round(x*percent), round(y*percent))
+    output = image.resize(new_dimensions, Image.ANTIALIAS)
+
+    # create file name variable that will have prefix "small_" that will be binded to new directory
+    # save "output" image as named by the output_file_name as JPEG in new directory
+    output_file_name = os.path.join(new_directory, "small_" + file_name)
+    output.save(output_file_name, "JPEG", quality = 95)
+
+# after everything is complete
+print("All done")
